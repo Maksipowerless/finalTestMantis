8f2ec7aa7f4e2cf41152615f56ba65b3bf4d4f33
max_from_perek <maksipowerless@mail.ru>
2017-05-23 14:07:15 +0300

    last

---
+++ config.groovy
@@ -0,0 +1,14 @@
+node {
+    git url: 'https://github.com/Maksipowerless/finalTestMantis', branch: 'master'
+//  def mvnHome = tool 'M3'
+    sh "mvn clean test"
+    step([$class: 'CucumberReportPublisher',
+          failedFeaturesNumber: 0,
+          failedScenariosNumber: 0,
+          failedStepsNumber: 0, fileExcludePattern: '',
+          fileIncludePattern: '**/mainReport.json',
+          jsonReportDirectory: '',
+          pendingStepsNumber: 0,
+          skippedStepsNumber: 0,
+          undefinedStepsNumber: 0])
+}
